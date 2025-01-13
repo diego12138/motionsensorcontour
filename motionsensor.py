7105beda62055e368199c135294c0acc49ef7091
diego12138 <diegoeduardo3004@gmail.com>
2025-01-12 23:16:57 -0500

    Delete requirements.txt and .devcontainer

--- motionsensor.py
+++ motionsensor.py
@@ -1,19 +1,7 @@
 import cv2
-import streamlit as st
-import numpy as np
-
-st.title('Motion Sensor')
-run = st.checkbox('Start Sensing')
-FRAME_WINDOW = st.image([])
 cap = cv2.VideoCapture(0)
 bg_sub = cv2.createBackgroundSubtractorMOG2()   
-
-while run:
-    if not run:
-        break
-    if not ret or frame is None:
-        st.warning("Unable to capture video frame. Check your camera or permissions.")
-        continue
+while True:
     ret, frame = cap.read()
     
     fgmask = bg_sub.apply(frame)
@@ -24,7 +12,12 @@ while run:
             x, y, w, h = cv2.boundingRect(contour)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     
-    FRAME_WINDOW.image(frame, channels='BGR')
+    cv2.imshow('original', frame)
+    cv2.imshow('fg', fgmask)
+    
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
     
 cap.release()
 cv2.destroyAllWindows()
